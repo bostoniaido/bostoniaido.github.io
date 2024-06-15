ba31092ab08489c1fbc00b42d79679e1d50f7bea
mercurial1027 <cmtaylor978@gmail.com>
2024-06-14 22:18:33 -0400

    Added terminology PDF to Existing Students FAQ

--- switch_view.js
+++ switch_view.js
@@ -400,16 +400,15 @@ function load_page(page) {
     </div>
     <div class="accordion-item">
       <h2 class="accordion-header" id="headingThree">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
           Been practicing with us for a while?
         </button>
       </h2>
       <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
         <div class="accordion-body">
-          <strong>Class Terminology</strong> <p> Coming Soon!</p>
+          <strong>Terminology, Definitions, Ranks and Titles</strong>
+          <p><a href="iaido_terminology.pdf">Download here</a></p>
           <strong>Etiquette</strong>
-          <p class="lh-base"> Coming Soon! </p>
-          <strong>Ranks</strong>
           <p class="lh-base"> Coming Soon! </p>
           <strong>Code of Conduct</strong>
           <p class="lh-base"> Coming Soon! </p>
