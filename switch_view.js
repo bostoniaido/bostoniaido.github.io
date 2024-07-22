c7e35e174291893c9cd4ee2dd72f3b5c0d9661e1
Tien Dao <thientien.dao@gmail.com>
2024-07-21 23:53:19 -0400

    Not gonna give you up

--- switch_view.js
+++ switch_view.js
@@ -45,9 +45,9 @@ function load_page(page) {
         // document.getElementById("subjectline").value = "Boston Iaido Interest! - " + d;
         document.querySelector("subjectline").value += " " + values.join();
       });
-      document.querySelector("subjectline").value += "Boston Iaido Interest! - " + values.join();
+      const constructStr = "Boston Iaido Interest! - " + values.join();
+      document.querySelector("subjectline").value = constructStr;
     });
-    document.querySelector("subjectline").value += "Boston Iaido Interest! - " + values.join();
   };
 
   if (page == "contact") {
